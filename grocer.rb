7fa3a9a1986e7772bcb4a729e57e6613cc376b99
Aaron Minyard <amnyrrd@gmail.com>
2020-01-22 23:07:02 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -74,6 +74,6 @@ def checkout(cart, coupons)
   total = 0
   count = 0
   while count < final_cart.length
-    total += final_cart[count][:price] * final_cart[count][:num]
+    total += final_cart[count][:price] * final_cart[count][:count]
   end
 end
