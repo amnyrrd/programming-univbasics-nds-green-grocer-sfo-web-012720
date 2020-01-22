ddf69856aa996c6b3d465e33b467915012046fbb
Aaron Minyard <amnyrrd@gmail.com>
2020-01-22 23:00:40 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -59,7 +59,7 @@ def apply_clearance(cart)
   count = 0
   while count < cart.length
     if cart[count][:clearance]
-      cart[count][:price] = cart[count][:price] - (cart[count][:price] *) 
+      cart[count][:price] = cart[count][:price] - (cart[count][:price] * 0.2) 
     end
   count += 1
   end
